726d0826732a6303b688ebc0b31e801137b22ec1
yuma-matsui <oshino.iiyo@gmail.com>
2022-07-30 13:21:52 +0900

    v-ifの練習

--- components/application.js
+++ components/application.js
@@ -1,35 +1,10 @@
 const app = Vue.createApp({
   data () {
     return {
-      count: 1,
-      isButtonDisabled: true,
-      items: [
-        { id: 1, text: 'myItem-1' },
-        { id: 2, text: 'myItem-2' },
-        { id: 3, text: 'myItem-3' },
-        { id: 4, text: 'myItem-4' },
-        { id: 5, text: 'myItem-5' }
-      ]
+      items: []
     }
   },
   methods: {
-    countUp () {
-      this.count++
-      console.log(this.count)
-    },
-
-    toggleButton () {
-      this.isButtonDisabled = !this.isButtonDisabled
-    },
-
-    toTitleDate (date) {
-      return new Date(date).toLocaleDateString()
-    },
-
-    formatDate (date) {
-      return `今日は${this.toTitleDate(date)}です。`
-    },
-
     addItem () {
       const id = this.items.length + 1
       const text = `myItem-${id}`
@@ -38,6 +13,10 @@ const app = Vue.createApp({
   },
 
   computed: {
+    existItem () {
+      return this.items.length > 0
+    },
+
     oddItems () {
       return [...this.items].filter(item => item.id % 2 === 1)
     },
